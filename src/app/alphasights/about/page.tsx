582c12a8f8079ab00aa551feef33fa9e43f9d876
willarrowsmith <will.arrowsmith15@gmail.com>
2025-02-21 11:39:47 +0000

    alpha

---
+++ src/app/alphasights/about/page.tsx
@@ -0,0 +1,11 @@
+import AboutContent from "@/app/alphasights/_components/AboutContent";
+import Sidebar from "@/app/alphasights/_components/Sidebar";
+
+export default function AboutPage() {
+  return (
+    <>
+      <Sidebar />
+      <AboutContent />
+    </>
+  );
+}
